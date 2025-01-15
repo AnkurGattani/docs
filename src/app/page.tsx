57835a35d7a706323382b6cd7e0688516148f888
Ankur Gattani <ankurgattani02@gmail.com>
2025-01-16 01:52:09 +0530

    add documents page and implement dynamic document ID routing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import Link from "next/link";
+
 const Home = () => {
   return (
     <div className="flex min-h-screen items-center justify-center">
-      <p>Hello World!</p>
+      {/* Home page with recent documents. Feature to be coded in future. */}
+      Click &nbsp; <Link href="/documents/123" className="text-blue-600 underline">  here </Link>&nbsp; to go to the documents page.
     </div>
   );
 }
