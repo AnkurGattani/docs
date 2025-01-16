c5694c88eb0176a57ecce2a350420b60973b509b
Ankur Gattani <ankurgattani02@gmail.com>
2025-01-16 20:09:52 +0530

    add Tiptap extensions for lists, tables, tasks, and images in the editor

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -2,6 +2,14 @@
 
 import { useEditor, EditorContent } from '@tiptap/react'
 import StarterKit from '@tiptap/starter-kit'
+import TaskItem from '@tiptap/extension-task-item'
+import TaskList from '@tiptap/extension-task-list'
+import Table from '@tiptap/extension-table'
+import TableCell from '@tiptap/extension-table-cell'
+import TableHeader from '@tiptap/extension-table-header'
+import TableRow from '@tiptap/extension-table-row'
+import Image from '@tiptap/extension-image'
+import ImageResize from 'tiptap-extension-resize-image'
 
 
 export const EditorComponent = () => {
@@ -12,8 +20,33 @@ export const EditorComponent = () => {
                 class: 'focus:outline-none print:border-0 bg-white border border-[#C7C7C7] flex flex-col min-h-[1054px] w-[816px] py-10 pr-14 cursor-text',
             }
         }, 
-        extensions: [StarterKit],
-        content: 'Hello World! 🌎️',
+        extensions: [
+            StarterKit,
+            Image,
+            ImageResize,
+            Table, 
+            TableCell,
+            TableHeader,
+            TableRow,
+            TaskList, 
+            TaskItem.configure({
+                nested: true,
+            })
+        ],
+        content: `<table>
+        <tbody>
+          <tr>
+            <th>Name</th>
+            <th colspan="3">Description</th>
+          </tr>
+          <tr>
+            <td>Cyndi Lauper</td>
+            <td>Singer</td>
+            <td>Songwriter</td>
+            <td>Actress</td>
+          </tr>
+        </tbody>
+      </table>`,
     })
     
     return (
