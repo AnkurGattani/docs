01343973c9eebc7ca24f9e5195b08d617bb9308c
Ankur Gattani <ankurgattani02@gmail.com>
2025-01-17 13:14:46 +0530

    add font family and heading options to the editor toolbar

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -12,6 +12,8 @@ import Image from '@tiptap/extension-image'
 import ImageResize from 'tiptap-extension-resize-image'
 import { useEditorStore } from '@/store/use-editor-store';
 import Underline from '@tiptap/extension-underline'
+import FontFamily from '@tiptap/extension-font-family'
+import TextStyle from '@tiptap/extension-text-style';
 
 import { useLiveblocksExtension } from "@liveblocks/react-tiptap";
 import { Threads } from './threads';
@@ -58,6 +60,8 @@ export const EditorComponent = () => {
             StarterKit.configure({
                 history: false,
             }),
+            FontFamily,
+            TextStyle,
             Underline,
             Image,
             ImageResize,
